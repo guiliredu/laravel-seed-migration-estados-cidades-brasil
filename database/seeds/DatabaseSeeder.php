ec43ae44e338783802b9a9b659e64944e20cbcee
Guilherme Dias Redü <guilisk8@gmail.com>
2019-02-27 16:57:54 -0300

    Cria base para pacote do Laravel

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
+namespace Guiliredu\BrazilianCityMigrationSeed\Database\Seeds;
 
 use Illuminate\Database\Seeder;
+use Guiliredu\BrazilianCityMigrationSeed\Database\Seeds\CityTableSeeder;
+use Guiliredu\BrazilianCityMigrationSeed\Database\Seeds\StateTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
